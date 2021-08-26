2ba91ee73fc0283de5714fd0ce4face512c60cad
RubenNaudts <naudtsruben@live.be>
2021-08-26 16:24:44 +0200

    Separate loadSlashCommand from index.js

--- index.js
+++ index.js
@@ -22,41 +22,6 @@ for (const file of messageCommandFiles) {
 
 
 
-//Register slash commands
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
-
-const clientId = '876761713968558120';
-const guildId = '755107000882429963';
-
-const commands = [];
-const slashCommandFiles = fs.readdirSync('./slashCommands').filter(file => file.endsWith('.js'));
-
-for (const file of slashCommandFiles) {
-	const command = require(`./slashCommands/${file}`);
-	commands.push(command.data.toJSON());
-}
-
-const rest = new REST({ version: '9' }).setToken(token);
-
-(async () => {
-	try {
-		console.log('Started refreshing application (/) commands.');
-
-		await rest.put(
-			Routes.applicationGuildCommands(clientId, guildId),
-			{ body: commands },
-		);
-
-		console.log('Successfully reloaded application (/) commands.');
-	} catch (error) {
-		console.error(error);
-	}
-})();
-
-
-
-
 //prints message to console once the client is ready
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
---
+++ loadSlashCommands.js
@@ -0,0 +1,31 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { token } = require('./config.json');
+const fs = require('fs');
+
+const clientId = '875728074979827824';
+
+const commands = [];
+const slashCommandFiles = fs.readdirSync('./slashCommands').filter(file => file.endsWith('.js'));
+
+for (const file of slashCommandFiles) {
+	const command = require(`./slashCommands/${file}`);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '9' }).setToken(token);
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationCommands(clientId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
