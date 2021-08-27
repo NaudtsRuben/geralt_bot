a32fbf25652f267c94bf8e44a2dac6fb8b6b3970
RubenNaudts <naudtsruben@live.be>
2021-08-27 23:45:27 +0200

    Remove disable-channel file

--- slashCommands/disable-channel.js
+++
@@ -1,7 +0,0 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-
-module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('disable-channel')
-		.setDescription('Disables the bot for the current channel')
-};
